2033bb3e1c23b29cfa705730d919189f49ae1aef
Sparsh Arora <85572129+sparsh7103@users.noreply.github.com>
2023-09-22 16:41:28 +0000

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Day
-* Updated: Aug 17 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/day-multipurpose-html-template-for-free/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
