c6fdeec1355726ec21cfc3a8ad8600ef5efd4811
Parth195 <89126978+Parth195@users.noreply.github.com>
2023-12-02 14:08:15 +0000

    full

--- session.js
+++ session.js
@@ -1,14 +1,11 @@
-// Function to set the user session
 function setUserSession() {
     localStorage.setItem('userLoggedIn', 'true');
 }
 
-// Function to clear the user session
 function clearUserSession() {
     localStorage.removeItem('userLoggedIn');
 }
 
-// Function to check if the user is logged in
 function isUserLoggedIn() {
     return localStorage.getItem('userLoggedIn') === 'true';
 }
